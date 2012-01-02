b8f6fc039c69e1a408f23a5708deb413dcd7aa47
jowebb <jowebb@gmail.com>
2012-01-01 18:00:45 -0800

    Exercise 1: Very easy. Took as long to decide how to represent items in Extra Credit section, as to do exercise, i.e. not long.

---
+++ ex1.rb
@@ -0,0 +1,16 @@
+#Exercise 1 
+
+#With extra credit 2. & 3.
+
+# puts "Hello World!"
+puts "Hello again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts 'Yay! Printing.'
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+
+#With extra credit 1.
+
+# puts "It just says another line, so assume line = line of text."
+
